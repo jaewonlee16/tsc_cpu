60e313b146a3befca95b2d657d49f3be8d8968a0
jaewonlee16 <19snut@gmail.com>
2023-04-30 13:40:10 +0900

    skeletone code

---
+++ pipeline/cpu.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ns
+`define WORD_SIZE 16    // data and address word size
+
+`include "opcodes.v"
+
+module cpu(
+        input Clk, 
+        input Reset_N, 
+
+	// Instruction memory interface
+        output i_readM, 
+        output i_writeM, 
+        output [`WORD_SIZE-1:0] i_address, 
+        inout [`WORD_SIZE-1:0] i_data, 
+
+	// Data memory interface
+        output d_readM, 
+        output d_writeM, 
+        output [`WORD_SIZE-1:0] d_address, 
+        inout [`WORD_SIZE-1:0] d_data, 
+
+        output [`WORD_SIZE-1:0] num_inst, 
+        output [`WORD_SIZE-1:0] output_port, 
+        output is_halted
+);
+
+	// TODO : Implement your pipelined CPU!
+
+endmodule
