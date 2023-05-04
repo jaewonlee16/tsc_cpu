2bcaaa8f861be907b317772a61603164c5db570a
jaewonlee16 <19snut@gmail.com>
2023-05-05 02:46:09 +0900

    not finished

--- pipeline/branch_predictor.v
+++ pipeline/branch_predictor.v
@@ -6,7 +6,7 @@ module branch_predictor
     input                      update_tag, // update tag as soon as decode (when target is known)
     input                      update_bht, // update BHT when know prediction was correct or not
     input [WORD_SIZE-1:0]      pc, // the pc that was just fetched
-    input [WORD_SIZE-1:0]      pc_tag_for_btb_update, // PC of the branch which just caused tag collision
+    input [WORD_SIZE-1:0]      pc_for_btb_update, // PC collision tag 
                                                 // always pc_id
     input [WORD_SIZE-1:0]      pc_real,        // The actual pc that is calculated (not predicted)
                                                // pc_ex(i type branch) or pc_id(jump)
@@ -16,3 +16,23 @@ module branch_predictor
     output                     tag_match, // tag matched PC
     output reg [WORD_SIZE-1:0] branch_predicted_pc // predicted next PC
 );
+
+   // Tag table
+   reg [WORD_SIZE-BTB_IDX_SIZE-1:0] tag_table[2**BTB_IDX_SIZE-1:0];
+   // Branch history table
+   reg [1:0] bht[2**BTB_IDX_SIZE-1:0];
+   // Branch target buffer
+   reg [BTB_IDX_SIZE-1:0] btb[2**BTB_IDX_SIZE-1:0];
+   // BTB index
+   wire [BTB_IDX_SIZE-1:0] btb_idx;
+   assign btb_idx = pc[BTB_IDX_SIZE-1:0];
+   // PC tag
+   wire [WORD_SIZE-BTB_IDX_SIZE-1:0] pc_tag;
+   assign pc_tag = pc[WORD_SIZE-1:BTB_IDX_SIZE];
+   // BTB hit
+   assign tag_match = (tags[btb_idx] == pc_tag);
+
+   wire [BTB_IDX_SIZE-1:0]           btb_idx_collided;
+   assign btb_idx_collided = pc_for_btb_update[BTB_IDX_SIZE-1:0];
+   wire [WORD_SIZE-BTB_IDX_SIZE-1:0] pc_tag_collided;
+   assign pc_tag_collided = pc_for_btb_update[WORD_SIZE-1:BTB_IDX_SIZE];
