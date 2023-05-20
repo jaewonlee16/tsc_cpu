abdbbad395fb1674327cd40d1f74b2e942cdf163
jaewonlee16 <19snut@gmail.com>
2023-05-20 12:33:47 +0900

    port name revised

--- cache/cache.v
+++ cache/cache.v
@@ -4,17 +4,16 @@ module cache
     input reset_n,
     input read_cache,
     input write_cache,
-    input [WORD_SIZE-1:0]      address_cache,
-    inout [WORD_SIZE-1:0]      data_cache_datapath, // data connected to datapath
-    inout [READ_SIZE-1:0]      data_mem_cache, // data connected to memory
+    input [WORD_SIZE-1:0] address_cache,
+    inout [WORD_SIZE-1:0] data_cache_datapath, // data connected to datapath
+    inout [READ_SIZE-1:0] data_mem_cache, // data connected to memory
 
-    // issue memory access for cache misses
-    output                      doneWrite,
-    output [WORD_SIZE-1:0]     addressM,
-    output reg                 readM,
-    output reg                 writeM,
-    output reg [WORD_SIZE-1:0] num_cache_access,
-    output reg [WORD_SIZE-1:0] num_cache_miss,
+    output doneWrite,  // tells the cpu that writing is finshed
+    output [WORD_SIZE-1:0] address_memory,
+    output reg readM,
+    output reg writeM,
    );
-    reg [WORD_SIZE-1:0] num_cache_access,
-    reg [WORD_SIZE-1:0] num_cache_miss,
+    reg [WORD_SIZE-1:0] num_cache_access,    // for debugging
+    reg [WORD_SIZE-1:0] num_cache_miss,      // for debugging
+
+    
